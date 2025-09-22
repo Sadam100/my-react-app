e36ff810f81f060997ca33e02285d01ca6f4c9d4
Sadam Hussain <sadamhussain011991@gmail.com>
2025-09-22 20:35:35 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello World from Sadam Hussain</h1>
+      <p className="read-the-docs">
+        Course: Cloud Computing
+      </p>
+    </>
+  )
+}
+
+export default App
